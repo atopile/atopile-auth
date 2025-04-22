606346071d8bd44996f49b4f0370253b6a634604
Matt Wildoer <matt@atopile.io>
2025-04-21 19:35:11 -0700

    Github auth working, project structure improved. Time to test, and publish

---
+++ examples/client.py
@@ -0,0 +1,44 @@
+import json
+import os
+
+import httpx
+import supabase
+import typer
+from supabase.lib.client_options import ClientOptions
+import atopile_auth
+import atopile_auth.client
+from atopile_auth.data_dir_storage import SyncDataDirStorage
+
+
+client = supabase.create_client(
+    supabase_url=os.getenv("SUPABASE_URL"),
+    supabase_key=os.getenv("SUPABASE_KEY"),
+    options=ClientOptions(storage=SyncDataDirStorage("atopile")),
+)
+
+app = typer.Typer()
+
+
+@app.command()
+def login(
+    oauth: str | None = None,
+    email: str | None = None,
+    password: str | None = None,
+):
+    atopile_auth.client.login(client, oauth=oauth, email=email, password=password)
+    print("Logged in.")
+
+
+@app.command()
+def demo():
+    auth = client.auth
+    response = httpx.get(
+        "http://localhost:8000/authed",
+        headers={"Authorization": f"Bearer {auth.get_session().access_token}"},
+    )
+    response.raise_for_status()
+    print(json.dumps(response.json(), indent=2))
+
+
+if __name__ == "__main__":
+    app()
